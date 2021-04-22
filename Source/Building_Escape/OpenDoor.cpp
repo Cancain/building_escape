be0f1b1eecb2dbe99380426dfb7adbc30863f6c1
Cancain <eriksson.tomas@gmail.com>
2021-04-22 20:22:00 +0200

    Lecture 104

--- Source/Building_Escape/OpenDoor.cpp
+++ Source/Building_Escape/OpenDoor.cpp
@@ -1,27 +1,17 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "OpenDoor.h"
 #include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
-UOpenDoor::UOpenDoor(){
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+UOpenDoor::UOpenDoor(){ PrimaryComponentTick.bCanEverTick = true; }
 
-	// ...
-}
-
-// Called when the game starts
 void UOpenDoor::BeginPlay(){
 	Super::BeginPlay();
 
 	InitalYaw = GetOwner()->GetActorRotation().Yaw;
 	CurrentYaw = InitalYaw;
-	TargetYaw = InitalYaw + 90.f;
+	TargetYaw += InitalYaw;
 }
 
-// Called every frame
 void UOpenDoor::TickComponent(const float DeltaTime, const ELevelTick TickType,
                               FActorComponentTickFunction* ThisTickFunction){
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
--- Source/Building_Escape/OpenDoor.h
+++ Source/Building_Escape/OpenDoor.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -11,21 +9,20 @@ class BUILDING_ESCAPE_API UOpenDoor : public UActorComponent{
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this component's properties
 	UOpenDoor();
 
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
 public:
-	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
 	                           FActorComponentTickFunction* ThisTickFunction) override;
 
 private:
-	float TargetYaw;
 	float CurrentYaw;
 	float InitalYaw;
+
+	UPROPERTY(EditAnywhere)
+	float TargetYaw{90.f};
 
 };
